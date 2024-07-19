ecf0d84ec2bebc011735e118658e0696f903a0c3
Emanoel Augusto <emanoelaugusto7@gmail.com>
2024-07-19 18:08:01 -0300

    feat: new info about me

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -30,13 +30,13 @@ export default function About() {
                     <section className="flex flex-col mt-20 gap-1">
                         <h3 className="font-bold text-white">Mais sobre mim</h3>
                         <p className="text-gray-200">
-                            Desde cedo sempre tive a vontade de criar coisas, criar algo que pessoas utilizassem, que pessoas dedicassem tempo a ver algo que participei para criar e vi que no desenvolvimento poderia criar qualquer coisa. Não sei se vocês já ouviram falar sobre Construct 2, esse era um programa que permitia que você criasse jogos 2D a partir de eventos, e assim era possível criar várias funcionalidades dentro do jogo.
+                            Desde cedo, sempre tive uma grande vontade de criar coisas — algo que as pessoas pudessem usar e dedicar seu tempo a apreciar. Descobri que no campo do desenvolvimento, eu poderia criar praticamente qualquer coisa. Meu primeiro contato com a &quot;Programação&quot; foi através do Construct 2, um programa que permitia criar jogos 2D usando eventos para implementar várias funcionalidades dentro do jogo.
                         </p>
                         <p className="text-gray-200">
-                            Esse foi meu primeiro contato com &quot;Programação&quot;, e me fazia muito bem, todos os dias depois da escola sempre dedicava um tempo para ver vídeos no youtube, tutorias. E nessas pesquisas foi aparecendo para mim outros tipos de criar algo, e foi assim que comecei a conhecer sobre Html, Css, Javascript e a partir desse ponto comecei cada vez mais a se apaixonar por programação. Depois de aprender o básico sobre Html e Javascript, comecei a conhecer os Frameworks e fiquei muito impressionado como aquilo poderia criar infinitas possibilidades.
+                            Esse foi um ponto de virada para mim. Após a escola, dedicava meu tempo assistindo vídeos no YouTube e tutoriais, mergulhando cada vez mais nesse universo fascinante. Foi durante essas pesquisas que descobri outras formas de criar, como HTML, CSS e Javascript. A partir desse ponto, minha paixão pela programação só cresceu. Dominar o básico dessas linguagens me levou a explorar Frameworks, e fiquei impressionado com as infinitas possibilidades que eles oferecem.
                         </p>
                         <p className="text-gray-200">
-                            Foi assim que comecei na programação, hoje vejo o quão importante foi ter curiosidade para descobrir coisas novas e creio que essa seja a chave para evoluir, buscar conhecimento e colocar em prática.
+                            Assim, foi como me iniciei na programação. Olhando para trás, percebo o quanto foi importante ter essa curiosidade incessante por descobrir coisas novas. Acredito que essa curiosidade é a chave para evoluirmos, buscarmos conhecimento e colocá-lo em prática no nosso dia a dia.
                         </p>
                     </section>
 
@@ -57,31 +57,31 @@ export default function About() {
                             </div>
                         </div>
                         <p className="text-gray-200">
-                            Meu primeiro emprego como programador, iniciei no modo colaborativo no projeto, consegui me destacar e depois fui contratado para trabalhar full-time no projeto.
+                            Sistema descentralizado de regeneração da natureza. A Sintrop está criando o Crédito de Regeneração, um inovador criptoativo de recompensa por serviços ambientais e uma nova forma de investir em carbono e na regeneração de ecossistemas, utilizando a blockchain para armazenamento e processamentos dos dados sem uma entidade central.
                         </p>
                         <p className="text-gray-200">
-                            Eu atuo no desenvolvimento do Front-End (Website, plataforma web e aplicativo móvel):
+                            Eu trabalho no desenvolvimento do Front-End, abrangendo website, plataforma web e aplicativo móvel:
                         </p>
 
                         <p className="text-gray-200 ml-2">
                             -Website:
                         </p>
                         <p className="text-gray-200 ml-5">
-                            Utilizamos NextJS no website, aproveitando todos os recursos de SEO e SSR (Server Side Rendering) que ele nos proporciona. Dentro do site também criamos um blog para postagens de conteúdos, utilizando o TipTap como editor de textos;
+                            Utilizamos Next.js para o website, aproveitando todos os recursos avançados de SEO e SSR (Server Side Rendering) que ele oferece. Implementamos um blog dentro do site utilizando o editor de textos TipTap.
                         </p>
 
                         <p className="text-gray-200 ml-2 mt-2">
                             -Plataforma Web:
                         </p>
                         <p className="text-gray-200 ml-5">
-                            Foi desenvolvida com ReactJS (CRA), e é nela onde a gente faz todas as interações com Smart Contracts da Blockchain da Ethereum.
+                            Desenvolvemos com React.js (CRA). Nesta plataforma, todas as interações com os Smart Contracts da Blockchain da Ethereum são realizadas.
                         </p>
 
                         <p className="text-gray-200 ml-2 mt-2">
                             -Aplicativo móvel:
                         </p>
                         <p className="text-gray-200 ml-5">
-                            Desenvolvido com React Native e Expo, o aplicativo foi pensado para promover a interação dos usuários como uma espécie de Rede Social, mas com foco na agricultura regenerativa. O aplicativo conta também com um modo offline, para que inspetores possam inspecionar produtores que não tenham conexão com a internet.
+                            Desenvolvido com React Native e Expo, o aplicativo é projetado para facilitar a interação dos usuários em uma espécie de Rede Social focada na agricultura regenerativa. Ele inclui um modo offline para permitir que inspetores realizem inspeções em produtores sem conexão com a internet.
                         </p>
 
                         <p className="text-gray-200 mt-2">
@@ -107,9 +107,6 @@ export default function About() {
                             />
                         </div>
                     </section>
-
-                    <h3 className="font-bold text-white mt-10">O que gosto de fazer</h3>
-                    <p className="text-gray-200 mt-1">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
                 </div>
             </main>
 
