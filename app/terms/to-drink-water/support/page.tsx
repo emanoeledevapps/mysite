cbe2d788425ca207cd48653de99e1a9efb9ac738
Emanoel Augusto <emanoelaugusto7@gmail.com>
2025-09-09 16:37:50 -0300

    feat: add terms and support of to drink water

---
+++ app/terms/to-drink-water/support/page.tsx
@@ -0,0 +1,37 @@
+export default function Support() {
+  return (
+    <>
+      <header className="flex flex-col container mx-auto py-10 px-5">
+        <h1 className="text-4xl">Beber água - Suporte</h1>
+      </header>
+
+      <main className="mt-10 flex flex-col container mx-auto px-5">
+        <p className="mt-5">
+          Precisa de ajuda ou quer tirar dúvidas sobre o uso do{" "}
+          <b>Beber água</b>? Nossa equipe está pronta para auxiliar você.
+        </p>
+
+        <p className="mt-5">
+          O app foi desenvolvido para ser simples, seguro e direto, permitindo
+          consultar CEPs sem complicações, cadastro ou permissões. Se encontrar
+          qualquer problema, tiver sugestões de melhorias ou precisar de suporte
+          técnico, entre em contato pelo e-mail abaixo:
+        </p>
+
+        <h3 className="font-bold mt-10 mb-3">Contato de Suporte</h3>
+        <p>
+          <b>E-mail:</b> emanoelaugusto7@gmail.com
+        </p>
+
+        <p className="mt-5">
+          Normalmente respondemos em até 2 dias úteis. Obrigado por usar o{" "}
+          <b>Beber água</b>!
+        </p>
+      </main>
+
+      <footer className="mt-10 py-10">
+        <p className="text-center">emanoelaugusto7@gmail.com</p>
+      </footer>
+    </>
+  );
+}
